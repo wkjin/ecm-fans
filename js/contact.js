7e604699638ad04222a73eb3e75ae38ee4cc9ebf
wangkj <wangkj@yex360.com>
2019-08-05 17:36:21 +0800

    修改展示

--- js/contact.js
+++ js/contact.js
@@ -106,7 +106,7 @@ var pageContentArea={
             loadSecondCategory();
 
             //填充二级导航
-            // self.$parent.find(self._options.secondCategoryContainerSelector).html(template(self._options.secondCategoryTemplagteId, {categorysList: categoryData._child}));
+            self.$parent.find(self._options.secondCategoryContainerSelector + '-1').html(template(self._options.secondCategoryTemplagteId + '-1', {categorysList: categoryData._child}));
 
             //模拟点击栏目进行选中
             self._selectSecondCategory(self.showCategoryIndex);
@@ -119,7 +119,7 @@ var pageContentArea={
         var self = this;
 
         //从地址栏获取选中的栏目
-        self.$parent.find(self._options.secondCategoryContainerSelector).off('click').on('click', 'span', function(){
+        self.$parent.find(self._options.secondCategoryContainerSelector + '-1').off('click').on('click', 'span', function(){
             var $this = $(this);
             self._selectSecondCategory($this.data('index'));
         });
@@ -130,7 +130,9 @@ var pageContentArea={
         var self = this;
         var selectedClass = self._options.secondCategorySelectedClass;
         index = parseInt(index);
-        var secondCategoryList = self.$parent.find(self._options.secondCategoryContainerSelector).find('> a[data-index]');
+        var secondCategoryList = self.$parent.find(self._options.secondCategoryContainerSelector + '-1').find('> span[data-index]');
+        self.$parent.find(self._options.secondCategoryContainerSelector).find('> a[data-index="'+index+'"]').addClass(selectedClass).siblings().removeClass(selectedClass);
+        
         if(isNaN(index) || index >= secondCategoryList.length || index < 0){
             index = 0;
         }
